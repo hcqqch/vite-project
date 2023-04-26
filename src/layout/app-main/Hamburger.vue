36113b532aba2e5728655e0f188a7d4d432195c2
huchengqiang <hcqlove@163.com>
2023-04-26 15:36:34 +0800

    init

---
+++ src/layout/app-main/Hamburger.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="padding: 0 12px" @click="toggleClick">
+    <svg-icon icon-class="hamburger" :class="{ 'is-active': isActive }" class="hamburger-style" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SvgIcon from '@/icons/SvgIcon.vue'
+defineProps({
+  isActive: {
+    type: Boolean,
+    default: false
+  }
+})
+const emit = defineEmits(['toggleClick'])
+//切换左侧栏关闭和隐藏
+const toggleClick = () => {
+  emit('toggleClick')
+}
+</script>
+
+<style scoped lang="scss">
+.hamburger-style {
+  color: #2b2f3a;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+
+.hamburger-style.is-active {
+  transform: rotate(180deg);
+}
+</style>
